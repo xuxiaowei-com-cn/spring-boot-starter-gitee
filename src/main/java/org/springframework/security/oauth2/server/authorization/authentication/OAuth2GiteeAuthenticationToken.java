26ec803df48252148cd587c994b5ed0416a83368
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-12-26 19:53:08 +0800

    :bug: 修正验证

--- src/main/java/org/springframework/security/oauth2/server/authorization/authentication/OAuth2GiteeAuthenticationToken.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/authentication/OAuth2GiteeAuthenticationToken.java
@@ -9,9 +9,9 @@ package org.springframework.security.oauth2.server.authorization.authentication;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -94,7 +94,7 @@ public class OAuth2GiteeAuthenticationToken extends OAuth2AuthorizationGrantAuth
 			String appid, String code, String scope, String remoteAddress, String sessionId, String state,
 			String binding) {
 		super(OAuth2GiteeAuthenticationToken.GITEE, clientPrincipal, additionalParameters);
-		Assert.hasText(code, "appid 不能为空");
+		Assert.hasText(appid, "appid 不能为空");
 		Assert.hasText(code, "code 不能为空");
 		this.appid = appid;
 		this.code = code;
